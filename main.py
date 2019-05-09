27f1db80ce2250800ef5dc85c796585ecb4c9151
Arne Binder <arne.b.binder@gmail.com>
2019-05-09 18:54:56 +0200

    added notebook; minor changes

--- main.py
+++ main.py
@@ -17,17 +17,20 @@ import six
 ###############################################################################
 
 
-import imp
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import keras
 import innvestigate
 from innvestigate.applications import imagenet
+import innvestigate.utils.visualizations as ivis
+import utils
+import utils_imagenet as imgnetutils
 
-base_dir = os.path.dirname(__file__)
-utils = imp.load_source("utils", os.path.join(base_dir, "utils.py"))
-imgnetutils = imp.load_source("utils_imagenet", os.path.join(base_dir, "utils_imagenet.py"))
+#import imp
+#base_dir = os.path.dirname(__file__)
+#utils = imp.load_source("utils", os.path.join(base_dir, "utils.py"))
+#imgnetutils = imp.load_source("utils_imagenet", os.path.join(base_dir, "utils_imagenet.py"))
 
 
 ###############################################################################
@@ -35,26 +38,49 @@ imgnetutils = imp.load_source("utils_imagenet", os.path.join(base_dir, "utils_im
 ###############################################################################
 
 METHODS = {
-    'input': {},
+    'input': {#'f_visualize': imgnetutils.heatmap
+              },
+    'input_dummy': {  # 'f_visualize': imgnetutils.heatmap
+                     #'f_visualize': imgnetutils.bk_proj
+    },
     'pattern.net': {'net_args': ['patterns'],
-                    'f_visualize': imgnetutils.bk_proj
+                    #'f_visualize': imgnetutils.bk_proj
+                     #'f_visualize': imgnetutils.heatmap,
+                    #'f_visualize':imgnetutils.graymap,
                     },
     'pattern.attribution': {'net_args': ['patterns'],
-                            'f_visualize': imgnetutils.heatmap
+                            #'f_visualize': imgnetutils.heatmap
+                            #'f_visualize': imgnetutils.bk_proj,
+                            #'f_visualize':imgnetutils.graymap,
                             },
     'gradient': {'kwargs': {'postprocess': 'abs'},
-                 'f_visualize': imgnetutils.heatmap # imgnetutils.graymap #imgnetutils.bk_proj
+                 #'f_visualize': imgnetutils.bk_proj,
+                 # 'f_visualize': imgnetutils.heatmap,
+                 #'f_visualize':imgnetutils.graymap,
                  },
     'input_t_gradient': {'kwargs': {'postprocess': 'abs'},
-                         'f_visualize': imgnetutils.heatmap
+                         #'f_visualize': imgnetutils.bk_proj
+                         #'f_visualize': imgnetutils.heatmap
+                         #'f_visualize': imgnetutils.image,
+                        #'f_visualize':imgnetutils.graymap,
                          },
-    'pattern.net*gradient': {'f_visualize': imgnetutils.heatmap
+    'pattern.net*gradient': {#'f_visualize': imgnetutils.heatmap
+                             #'f_visualize': imgnetutils.image,
+                             #'f_visualize': imgnetutils.bk_proj,
+                             #'f_visualize':imgnetutils.graymap,
                              },
-    'input*gradient': {'f_visualize': imgnetutils.heatmap
+    'input*gradient': {#'f_visualize': imgnetutils.heatmap
+                       #'f_visualize': imgnetutils.image,
+                       #'f_visualize': imgnetutils.bk_proj,
+                      #'f_visualize':imgnetutils.graymap,
                       },
     'deep_taylor.bounded': {'net_getter_kwargs': {'low': lambda net: net["input_range"][0],
                                                   'high': lambda net: net["input_range"][1]},
-                            'f_visualize': imgnetutils.heatmap}
+                            #'f_visualize': imgnetutils.heatmap,
+                            #'f_visualize': imgnetutils.bk_proj,
+                            #'f_visualize':imgnetutils.graymap,
+                            },
+    #'gradient_projected' : {},
 }
 
 def create_vgg16_model_and_analyzer(methods_metadata, analyzer_names=('input', 'pattern.net',)):
@@ -162,7 +188,7 @@ def analyze(data, analyzers, net, model, label_to_class_name):
     return analysis, text
 
 # adapted from examples/notebook/imagenet_compare_methods.ipnb
-def visualize(analysis, text, method_names, methods_metadata):
+def visualize(analysis, text, method_names, visualization_transformations, file_name=None, figsize=None):
     # Apply analysis postprocessing, e.g., creating a heatmap.
     #postprocess = {i: METHODS[method_name].get('visualize', lambda x: x) for i, method_name in enumerate(method_names)}
     # Prepare the grid as rectangular list
@@ -175,8 +201,10 @@ def visualize(analysis, text, method_names, methods_metadata):
             _method_name = method_names[j]
             #_p_func = METHODS[_method_name].get('f_visualize', lambda x: x)
             grid[i].append(analysis[i, j])
-            if 'f_visualize' in methods_metadata[_method_name]:
-                _p_func = methods_metadata[_method_name]['f_visualize']
+            _p_func = visualization_transformations.get(_method_name, visualization_transformations.get('default', None))
+
+            if _p_func is not None:
+                #_p_func = methods_metadata[_method_name]['f_visualize']
                 # batch dim has to be added before f_visualize application (otherwise bk_proj fails)
                 grid[i][j] = _p_func(np.expand_dims(grid[i][j], axis=0))[0]
 
@@ -191,8 +219,21 @@ def visualize(analysis, text, method_names, methods_metadata):
     # Plot the analysis.
     utils.plot_image_grid(grid, row_labels_left, row_labels_right, col_labels,
                           #file_name=os.environ.get("plot_file_name", None),
-                          file_name=os.path.join('out', 'test_analysis')
+                          file_name=file_name,
+                          figsize=figsize
                           )
+
+def scale_max_batch(X, output_range=(0, 1), input_is_positive_only=False):
+    dims_reduce = tuple(np.arange(len(X.shape) - 1) + 1)
+    if input_is_positive_only is False:
+        X = (X+1)/2  # [0, 1]
+
+    X = X.clip(0, 1)
+    X_max = np.amax(X, axis=dims_reduce, keepdims=True)
+
+    #X = output_range[0] + (X * (output_range[1]-output_range[0]))
+    X = X / X_max
+    return X
 
 def main():
 
@@ -201,6 +242,7 @@ def main():
     #method_names=['input', 'pattern.net',]
     #method_names = ['input', 'gradient', 'pattern.net*gradient']
     method_names = ['input', 'gradient', 'deep_taylor.bounded', 'pattern.attribution', 'pattern.net', 'input_t_gradient']# 'input*gradient', 'pattern.net*gradient', ]
+    #method_names = ['input', 'gradient', 'pattern.net',]# 'input_t_gradient']
     model, net, analyzers = create_vgg16_model_and_analyzer(methods_metadata=METHODS,
         analyzer_names=method_names
 
@@ -212,13 +254,42 @@ def main():
         raise Exception("Please download the example images using: "
                         "'innvestigate/examples/images/wget_imagenet_2011_samples.sh'")
 
-    analysis, text = analyze(data=images, analyzers=analyzers, net=net, model=model, label_to_class_name=label_to_class_name)
+    analysis, text = analyze(data=images[:1], analyzers=analyzers, net=net, model=model, label_to_class_name=label_to_class_name)
     #visualize(analysis, text, method_names)
-    _get_analysis = {method_name: np.expand_dims(analysis[:,idx], axis=1) for idx, method_name in enumerate(method_names)}
-    gradient_input = _get_analysis['gradient'] * _get_analysis['input']
-    gradient_patternnet = _get_analysis['gradient'] * _get_analysis['pattern.net']
-    analysis = np.concatenate((analysis, gradient_input, gradient_patternnet), axis=1)
-    visualize(analysis, text, method_names + ['input*gradient', 'pattern.net*gradient'], methods_metadata=METHODS)
+    analysis_dict = {method_name: np.expand_dims(analysis[:,idx], axis=1) for idx, method_name in enumerate(method_names)}
+    #gradient_projected = scale_max_batch(analysis_dict['gradient'], input_is_positive_only=True)
+    analysis_dict['gradient*input'] = analysis_dict['gradient'] * analysis_dict['input']
+    #gradient_t_input = analysis_dict['input']
+    #patternnet_clip = np.clip(analysis_dict['pattern.net'], a_min=0, a_max=None)
+    analysis_dict['abs(pattern.net)'] = np.abs(analysis_dict['pattern.net'])
+    #method_names = method_names + ['abs(pattern.net)']
+    #gradient_t_patternnet = analysis_dict['gradient'] * analysis_dict['pattern.net']
+
+    # show value distribution for all methods
+    fig, axs = plt.subplots(len(analysis_dict), 1, sharey=True, tight_layout=True, figsize=(10, 15))
+    for i, method_name in enumerate(analysis_dict):
+        #print('{:15} [{:.5f}, {:.5f}]'.format(method_name, analysis[:, i].min(), analysis[:, i].max()))
+        data = analysis_dict[method_name].reshape((-1))
+        axs[i].hist(data, bins='auto', color='#0504aa', alpha=0.7)
+        axs[i].set_xlabel(method_name)
+        #axs[i].yaxis.set_label_position("right")
+    plt.show()
+
+    visualization_transformations = {
+        'input': None,
+        'gradient': imgnetutils.bk_proj,
+        'deep_taylor.bounded': None,
+        'pattern.attribution': None,
+        'pattern.net': imgnetutils.bk_proj,
+        'input_t_gradient': None,
+        'input*gradient': None,
+        'pattern.net*gradient': None,
+        'abs(pattern.net)': imgnetutils.bk_proj
+    }
+    visualize(np.concatenate([analysis_dict[mn] for mn in analysis_dict], axis=1),
+              text, list(analysis_dict),
+              visualization_transformations=visualization_transformations,
+              file_name=os.path.join('out', 'test_analysis'))
 
 if __name__ == "__main__":
     main()
